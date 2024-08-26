6d7e7b08f46043954cbd756cc6a294e07e52d915
almsam <44277999+almsam@users.noreply.github.com>
2024-08-26 14:52:16 -0700

    skeleton & data

---
+++ Plot finder.py
@@ -0,0 +1,20 @@
+import pandas as pd; import numpy as np; import seaborn as sns # type: ignore
+import statsmodels.api as sm; import statsmodels.formula.api as smf # type: ignore
+
+# Lets start out by adding a dataset
+df = pd.read_csv('possum.csv'); print(df.head(10))
+
+# Then we can do a linear regression
+# And then measure how accurate it is
+
+# Then we can do a quadratic regression
+# And then measure how accurate it is
+
+# Then we can do an exp regression
+# And then measure how accurate it is
+
+# & then a sign wave regression
+# And then measure how accurate it is
+
+# Then compare how accurate all of the above is
+# & designate the most accurate
