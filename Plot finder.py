aa410a3415add55ec37ceee6e6daab9f12e707d5
almsam <44277999+almsam@users.noreply.github.com>
2024-08-27 16:25:17 -0700

    format

--- Plot finder.py
+++ Plot finder.py
@@ -5,24 +5,21 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf  # type: ignore
 
 # Lets start out by adding a dataset
-df = pd.read_csv(
-    "c:/Users/samia/OneDrive/Desktop/Numerical-Analysis-code/data/possum.csv"
-)
-print(df.head(10))
-print("ran success")
+df = pd.read_csv("c:/Users/samia/OneDrive/Desktop/Numerical-Analysis-code/data/possum.csv")
+# print(df.head(10)); print("ran success")
 
-# Then we can do a linear regression
+#   Then we can do a linear regression
 df_filtered = df[["hdlngth", "age"]].dropna()
-y = df_filtered["hdlngth"]
-x = df_filtered["age"]
+y = df_filtered["hdlngth"]; x = df_filtered["age"]
+
 sm.add_constant(x)
 model = sm.OLS(y, x).fit()
+
 # And then measure how accurate it is
-print(model.summary2())
-pred = model.predict(x)
-residuals = y - pred
-mae = np.mean(np.abs(residuals))
-print("lin reg error: ", mae)
+pred = model.predict(x); residuals = y - pred
+LinearError = np.mean(np.abs(residuals))
+
+print("lin reg error: ", LinearError)
 
 
 # Then we can do a quadratic regression
