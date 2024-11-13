968efb002ab951b695f3575f727f7203c6baee95
almsam <44277999+almsam@users.noreply.github.com>
2024-11-13 14:44:21 -0800

    refactor

--- Plot_Finder_Test.py
+++ Plot_Finder_Test.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 from sympy import symbols, exp, log, sin
 import statsmodels.api as sm
 import statsmodels.formula.api as smf  # type: ignore
-from Plot_Finder import linear_regression, quadratic_regression, cubic_regression, poly_regression, exp_regression, logarithmic_regression, sin_regression, logistic_regression, loess_regression, main
+from Plot_Finder import linear_regression, quadratic_regression, cubic_regression, poly_regression, exp_regression, logarithmic_regression, sin_regression, logistic_regression, loess_regression, find_best_fit
 
 class TestRegressionMethods(unittest.TestCase):
 
