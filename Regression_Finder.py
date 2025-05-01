0c76ef10c767249cf9e4ebe994bfcc889b87a818
almsam <44277999+almsam@users.noreply.github.com>
2025-04-30 18:18:25 -0700

    data

--- Regression_Finder.py
+++ Regression_Finder.py
@@ -3,3 +3,7 @@ import numpy as np
 import seaborn as sns  
 import matplotlib.pyplot as plt
 from sympy import symbols, sympify, solve, diff, lambdify, exp, log, sin
+
+df = pd.read_csv("c:/Users/samia/OneDrive/Desktop/Numerical-Analysis-code/data/possum.csv"); df_filtered = df[["hdlngth", "age"]].dropna()
+y = df_filtered["hdlngth"].values; x = df_filtered["age"].values; age = symbols('x')
+
