532412c28ee5edbd5bea6fa2bc0d844f3de0cf88
almsam <44277999+almsam@users.noreply.github.com>
2025-05-01 19:03:21 -0700

    setup

---
+++ Regression_Finder_Standards_Test.py
@@ -0,0 +1,12 @@
+import unittest
+import numpy as np
+from Regression_Finder import *
+from Regression_Standards import *
+
+class TestRegressionStandards(unittest.TestCase):
+
+    def setUp(self):
+        
+
+if __name__ == '__main__':
+    unittest.main()
