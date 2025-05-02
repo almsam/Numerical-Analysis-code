2db2b448bc51613766806baa4621cd8f68202fbc
almsam <44277999+almsam@users.noreply.github.com>
2025-05-01 19:28:35 -0700

    test log logic made

--- Regression_Finder_Standards_Test.py
+++ Regression_Finder_Standards_Test.py
@@ -12,14 +12,21 @@ class TestRegressionStandards(unittest.TestCase):
         cls.x = df_filtered["age"].values
         cls.y = df_filtered["hdlngth"].values
 
-    def check_standards_methods(self, model):
+    def check_standards_methods(self, model, model_name):
+        
+        print("\n \n ############################################################")
+        print("\n \n \t \t Model: " + model_name)
+
+        print("\n \n \n Printing all terms for model " + model_name)
         print_all_terms(model)
         
+        print("\n \n \n Printing all non 0 terms for model " + model_name)
         print_non_zero_terms(model)
         
-        # model.add_regression_outputs()
-        
+        print("\n \n \n Testing sympy func for model " + model_name)
         func = generate_sympy_function(model)
+        
+        # model.add_regression_outputs()
         
         # self.assertIsNotNone(func)
         # model.plot_function()
@@ -32,31 +39,31 @@ class TestRegressionStandards(unittest.TestCase):
 
     def test_linear_standards(self):
         error, regression = linear_regression(self.x, self.y)
-        self.check_standards_methods(regression)
+        self.check_standards_methods(regression, "linear")
 
     def test_quadratic_standards(self):
         error, regression = quadratic_regression(self.x, self.y)
-        self.check_standards_methods(regression)
+        self.check_standards_methods(regression, "quadratic")
 
     def test_cubic_standards(self):
         error, regression = cubic_regression(self.x, self.y)
-        self.check_standards_methods(regression)
+        self.check_standards_methods(regression, "cubic")
 
     def test_poly_standards(self):
         error, regression = poly_regression(self.x, self.y, 4)
-        self.check_standards_methods(regression)
+        self.check_standards_methods(regression, "poly")
 
     def test_sin_standards(self):
         error, regression = exp_regression(self.x, self.y)
-        self.check_standards_methods(regression)
+        self.check_standards_methods(regression, "sin")
 
     def test_log_standards(self):
         error, regression = logarithmic_regression(self.x, self.y)
-        self.check_standards_methods(regression)
+        self.check_standards_methods(regression, "log")
 
     def test_exponential_standards(self):
         error, regression = exp_regression(self.x, self.y)
-        self.check_standards_methods(regression)
+        self.check_standards_methods(regression, "exp")
 
 
 if __name__ == '__main__':
