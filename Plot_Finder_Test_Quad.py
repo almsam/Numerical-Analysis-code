72acb5d46f6172dafc3e7ea665da88ac7a7b237e
almsam <44277999+almsam@users.noreply.github.com>
2025-05-23 16:51:52 -0700

    labels

--- Plot_Finder_Test_Quad.py
+++ Plot_Finder_Test_Quad.py
@@ -124,6 +124,7 @@ class TestRegressionMethods(unittest.TestCase):
 ######################### p4 #################################
 
     def test_polynomial_p4_q1(self):
+        print("\n\n\n Poly4 Q1:\n\n\n")
         x = np.linspace(0.1, 3, 50)
         y = x**4 + x**2 + 1
         method, error, formula = find_best_fit(x, y, True)
@@ -131,6 +132,7 @@ class TestRegressionMethods(unittest.TestCase):
         print("P4 Q1:\nexpected:", "x**4 + x**2 + 1", "\nreceived:", str(formula))
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for p4q1 data")
     def test_polynomial_p4_q2(self):
+        print("\n\n\n Poly4 Q2:\n\n\n")
         x = np.linspace(-3, -0.1, 50)
         y = x**4 - 2 * x**2 + 1
         method, error, formula = find_best_fit(x, y, True)
@@ -138,6 +140,7 @@ class TestRegressionMethods(unittest.TestCase):
         print("P4 Q2:\nexpected:", "x**4 - 2*x**2 + 1", "\nreceived:", str(formula))
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for p4q2 data")
     def test_polynomial_p4_q3(self):
+        print("\n\n\n Poly4 Q3:\n\n\n")
         x = np.linspace(-3, -0.1, 50)
         y = -x**4 - x**2 - 2
         method, error, formula = find_best_fit(x, y, True)
@@ -145,6 +148,7 @@ class TestRegressionMethods(unittest.TestCase):
         print("P4 Q3:\nexpected:", "-x**4 - x**2 - 2", "\nreceived:", str(formula))
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for p4q3 data")
     def test_polynomial_p4_q4(self):
+        print("\n\n\n Poly4 Q4:\n\n\n")
         x = np.linspace(0.1, 3, 50)
         y = -x**4 + x**2 - 1
         method, error, formula = find_best_fit(x, y, True)
@@ -155,6 +159,7 @@ class TestRegressionMethods(unittest.TestCase):
 ######################### p5 #################################
 
     def test_polynomial_p5_q1(self):
+        print("\n\n\n Poly5 Q1:\n\n\n")
         x = np.linspace(0.1, 3, 50)
         y = x**5 + x**3 + x
         method, error, formula = find_best_fit(x, y, True)
@@ -162,6 +167,7 @@ class TestRegressionMethods(unittest.TestCase):
         print("P5 Q1:\nexpected:", "x**5 + x**3 + x", "\nreceived:", str(formula))
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for p5q1 data")
     def test_polynomial_p5_q2(self):
+        print("\n\n\n Poly5 Q2:\n\n\n")
         x = np.linspace(-3, -0.1, 50)
         y = -x**5 + x**3 - x
         method, error, formula = find_best_fit(x, y, True)
@@ -169,6 +175,7 @@ class TestRegressionMethods(unittest.TestCase):
         print("P5 Q2:\nexpected:", "-x**5 + x**3 - x", "\nreceived:", str(formula))
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for p5q2 data")
     def test_polynomial_p5_q3(self):
+        print("\n\n\n Poly5 Q3:\n\n\n")
         x = np.linspace(-3, -0.1, 50)
         y = -x**5 - x**3 - x
         method, error, formula = find_best_fit(x, y, True)
@@ -176,6 +183,7 @@ class TestRegressionMethods(unittest.TestCase):
         print("P5 Q3:\nexpected:", "-x**5 - x**3 - x", "\nreceived:", str(formula))
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for p5q3 data")
     def test_polynomial_p5_q4(self):
+        print("\n\n\n Poly5 Q4:\n\n\n")
         x = np.linspace(0.1, 3, 50)
         y = x**5 - x**3 + x
         method, error, formula = find_best_fit(x, y, True)
@@ -186,6 +194,7 @@ class TestRegressionMethods(unittest.TestCase):
 ######################### p6 #################################
 
     def test_polynomial_p6_q1(self):
+        print("\n\n\n Poly6 Q1:\n\n\n")
         x = np.linspace(0.1, 3, 50)
         y = x**6 + x**4 + x**2
         method, error, formula = find_best_fit(x, y, True)
@@ -193,6 +202,7 @@ class TestRegressionMethods(unittest.TestCase):
         print("P6 Q1:\nexpected:", "x**6 + x**4 + x**2", "\nreceived:", str(formula))
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for p6q1 data")
     def test_polynomial_p6_q2(self):
+        print("\n\n\n Poly6 Q2:\n\n\n")
         x = np.linspace(-3, -0.1, 50)
         y = x**6 - x**4 + x**2
         method, error, formula = find_best_fit(x, y, True)
@@ -200,6 +210,7 @@ class TestRegressionMethods(unittest.TestCase):
         print("P6 Q2:\nexpected:", "x**6 - x**4 + x**2", "\nreceived:", str(formula))
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for p6q2 data")
     def test_polynomial_p6_q3(self):
+        print("\n\n\n Poly6 Q3:\n\n\n")
         x = np.linspace(-3, -0.1, 50)
         y = -x**6 - x**4 - x**2
         method, error, formula = find_best_fit(x, y, True)
@@ -207,6 +218,7 @@ class TestRegressionMethods(unittest.TestCase):
         print("P6 Q3:\nexpected:", "-x**6 - x**4 - x**2", "\nreceived:", str(formula))
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for p6q3 data")
     def test_polynomial_p6_q4(self):
+        print("\n\n\n Poly6 Q4:\n\n\n")
         x = np.linspace(0.1, 3, 50)
         y = -x**6 + x**4 - x**2
         method, error, formula = find_best_fit(x, y, True)
@@ -217,6 +229,7 @@ class TestRegressionMethods(unittest.TestCase):
 ######################### p7 #################################
 
     def test_polynomial_p7_q1(self):
+        print("\n\n\n Poly7 Q1:\n\n\n")
         x = np.linspace(0.1, 3, 50)
         y = x**7 + x**5 + x**3 + x
         method, error, formula = find_best_fit(x, y, True)
@@ -224,6 +237,7 @@ class TestRegressionMethods(unittest.TestCase):
         print("P7 Q1:\nexpected:", "x**7 + x**5 + x**3 + x", "\nreceived:", str(formula))
         self.assertAlmostEqual(error, 0, places=3, msg="Expected zero error for p7q1 data")
     def test_polynomial_p7_q2(self):
+        print("\n\n\n Poly7 Q2:\n\n\n")
         x = np.linspace(-3, -0.1, 50)
         y = -x**7 + x**5 - x**3 + x
         method, error, formula = find_best_fit(x, y, True)
@@ -231,6 +245,7 @@ class TestRegressionMethods(unittest.TestCase):
         print("P7 Q2:\nexpected:", "-x**7 + x**5 - x**3 + x", "\nreceived:", str(formula))
         self.assertAlmostEqual(error, 0, places=3, msg="Expected zero error for p7q2 data")
     def test_polynomial_p7_q3(self):
+        print("\n\n\n Poly7 Q3:\n\n\n")
         x = np.linspace(-3, -0.1, 50)
         y = -x**7 - x**5 - x**3 - x
         method, error, formula = find_best_fit(x, y, True)
@@ -238,6 +253,7 @@ class TestRegressionMethods(unittest.TestCase):
         print("P7 Q3:\nexpected:", "-x**7 - x**5 - x**3 - x", "\nreceived:", str(formula))
         self.assertAlmostEqual(error, 0, places=3, msg="Expected zero error for p7q3 data")
     def test_polynomial_p7_q4(self):
+        print("\n\n\n Poly7 Q4:\n\n\n")
         x = np.linspace(0.1, 3, 50)
         y = x**7 - x**5 + x**3 - x
         method, error, formula = find_best_fit(x, y, True)
@@ -250,24 +266,28 @@ class TestRegressionMethods(unittest.TestCase):
 ######################### exp #################################
 
     def test_exp_q1(self):
+        print("\n\n\n Exp Q1:\n\n\n")
         x = np.linspace(0.1, 5, 50)
         y = 2 * np.exp(0.3 * x)
         method, error, formula = find_best_fit(x, y, True)
         self.assertEqual(method, "Exponential")
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for exp q1 data")
     def test_exp_q2(self):
+        print("\n\n\n Exp Q2:\n\n\n")
         x = -np.linspace(0.1, 5, 50)
         y = 2 * np.exp(0.3 * x)
         method, error, formula = find_best_fit(x, y, True)
         self.assertEqual(method, "Exponential")
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for exp q2 data")
     def test_exp_q3(self):
+        print("\n\n\n Exp Q3:\n\n\n")
         x = -np.linspace(0.1, 5, 50)
         y = -2 * np.exp(0.3 * x)
         method, error, formula = find_best_fit(x, y, True)
         self.assertEqual(method, "Exponential")
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for exp q3 data")
     def test_exp_q4(self):
+        print("\n\n\n Exp Q4:\n\n\n")
         x = np.linspace(0.1, 5, 50)
         y = -2 * np.exp(0.3 * x)
         method, error, formula = find_best_fit(x, y, True)
@@ -277,24 +297,28 @@ class TestRegressionMethods(unittest.TestCase):
 ######################### log #################################
 
     def test_log_q1(self):
+        print("\n\n\n Log Q1:\n\n\n")
         x = np.linspace(1, 10, 50)
         y = 2 * np.log(x) + 5
         method, error, formula = find_best_fit(x, y, True)
         self.assertEqual(method, "Logarithmic")
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for log q1 data")
     def test_log_q2(self):
+        print("\n\n\n Log Q2:\n\n\n")
         x = np.linspace(1, 10, 50)
         y = -2 * np.log(x) + 5
         method, error, formula = find_best_fit(x, y, True)
         self.assertEqual(method, "Logarithmic")
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for log q2 data")
     def test_log_q3(self):
+        print("\n\n\n Log Q3:\n\n\n")
         x = np.linspace(1, 10, 50)
         y = -2 * np.log(x) - 5
         method, error, formula = find_best_fit(x, y, True)
         self.assertEqual(method, "Logarithmic")
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for log q3 data")
     def test_log_q4(self):
+        print("\n\n\n Log Q4:\n\n\n")
         x = np.linspace(1, 10, 50)
         y = 2 * np.log(x) - 5
         method, error, formula = find_best_fit(x, y, True)
@@ -304,24 +328,28 @@ class TestRegressionMethods(unittest.TestCase):
 ######################### sin #################################
 
     def test_sin_q1(self):
+        print("\n\n\n Sin Q1:\n\n\n")
         x = np.linspace(0, np.pi / 2, 50)
         y = 4 * np.sin(x)
         method, error, formula = find_best_fit(x, y, True)
         self.assertEqual(method, "Sine")
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for sin q1 data")
     def test_sin_q2(self):
+        print("\n\n\n Sin Q2:\n\n\n")
         x = np.linspace(np.pi / 2, np.pi, 50)
         y = 4 * np.sin(x)
         method, error, formula = find_best_fit(x, y, True)
         self.assertEqual(method, "Sine")
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for sin q2 data")
     def test_sin_q3(self):
+        print("\n\n\n Sin Q3:\n\n\n")
         x = np.linspace(np.pi, 3 * np.pi / 2, 50)
         y = 4 * np.sin(x)
         method, error, formula = find_best_fit(x, y, True)
         self.assertEqual(method, "Sine")
         self.assertAlmostEqual(error, 0, places=5, msg="Expected zero error for sin q3 data")
     def test_sin_q4(self):
+        print("\n\n\n Sin Q4:\n\n\n")
         x = np.linspace(3 * np.pi / 2, 2 * np.pi, 50)
         y = 4 * np.sin(x)
         method, error, formula = find_best_fit(x, y, True)
