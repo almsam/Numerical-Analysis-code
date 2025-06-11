99cd7abc69e12d9992f6fca339b1ac5c04ada1ec
almsam <44277999+almsam@users.noreply.github.com>
2025-06-11 14:38:48 -0700

    refactor method name

--- Plot_Finder_Test_Fourier.py
+++ Plot_Finder_Test_Fourier.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 from sympy import symbols, lambdify, exp, log, sin
 import statsmodels.api as sm
 import statsmodels.formula.api as smf  # type: ignore
-from Plot_Finder import linear_regression, quadratic_regression, cubic_regression, poly_regression, exp_regression, logarithmic_regression, sin_regression, find_best_fit, plot_best_fit, fourier
+from Plot_Finder import find_best_fit, plot_best_fit, find_fourier
 # from Numerical_Methods import *
 import matplotlib.pyplot as plt
 
@@ -18,7 +18,7 @@ class TestFourierRegression(unittest.TestCase):
         self.age = symbols("age")
 
     def test_fourier_regression(self):
-        formula = fourier(self.x, self.y, 8, True)
+        formula = find_fourier(self.x, self.y, 8, True)
         # formula_func = lambdify(self.x, formula, 'numpy')
         y_range = lambdify(self.age, formula, 'numpy')(self.x)
         residuals = self.y.copy(); residuals = residuals - y_range
